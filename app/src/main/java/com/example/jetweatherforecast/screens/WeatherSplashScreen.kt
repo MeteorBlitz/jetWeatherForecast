afc7c8eb700d1ed30994dca18af119d3f98a7a92
deepak <dlipkumar77@gmail.com>
2025-03-28 17:33:46 +0530

    Creating UI - SplashScreen design

--- app/src/main/java/com/example/jetweatherforecast/screens/WeatherSplashScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/WeatherSplashScreen.kt
@@ -1,10 +1,39 @@
 package com.example.jetweatherforecast.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.jetweatherforecast.R
 
 @Composable
 fun WeatherSplashScreen(navController: NavController){
-    Text(text = "Weather App")
+    Surface(modifier = Modifier.padding(15.dp).size(300.dp),
+        shape = CircleShape,
+        border = BorderStroke(width = 2.dp, color = Color.LightGray) ) {
+        Column(modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Image(painter = painterResource(R.drawable.sun) , contentDescription = "sunny icon",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier.size(95.dp),)
+            Text(text = "Find the Sun?",
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.LightGray)
+        }
+    }
 }
