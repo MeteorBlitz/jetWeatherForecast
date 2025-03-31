dad82a995c7b448c2ef5c21f7d7ba4fd036a50ff
deepak <dlipkumar77@gmail.com>
2025-03-31 11:32:42 +0530

    setting up WeatherApi interface for retrofit

---
+++ app/src/main/java/com/example/jetweatherforecast/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.jetweatherforecast.utils
+
+object Constants {
+    //https://api.openweathermap.org/data/2.5/forecast/daily?q=lisbon&appid=ed60fc&units=metric
+
+    const val BASE_URL = "https://api.openweathermap.org/"
+    const val API_KEY = "ed60fc"
+}
