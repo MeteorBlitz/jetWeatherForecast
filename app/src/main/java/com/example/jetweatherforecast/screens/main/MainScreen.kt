2ce1b9365044c6b807d4b2914d5d84e0eab7b37f
deepak <dlipkumar77@gmail.com>
2025-03-31 18:13:20 +0530

    creating main screen widgets

--- app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/main/MainScreen.kt
@@ -1,20 +1,34 @@
 package com.example.jetweatherforecast.screens.main
 
+import android.media.Image
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter.State
+import coil3.compose.rememberAsyncImagePainter
 import com.example.jetweatherforecast.data.DataOrExceptional
 import com.example.jetweatherforecast.model.Weather
 import com.example.jetweatherforecast.widgets.WeatherAppBar
@@ -59,5 +73,41 @@ fun MainScaffold(weather: Weather, navController: NavController) {
 
 @Composable
 fun MainContent(data: Weather) {
-    Text(text = data.city.country)
+
+    val imageUrl = "https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png"
+    Column (modifier = Modifier.padding(4.dp).fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center){
+
+        Text(text = "Nov 29", style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+            modifier = Modifier.padding(6.dp),
+            fontWeight = FontWeight.SemiBold,)
+
+        Surface(modifier = Modifier.padding(4.dp).size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)) {
+            Column (verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally){
+
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(text = "56",
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.ExtraBold,)
+                Text(text = "Snow",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontStyle = FontStyle.Italic,)
+
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    AsyncImage(
+        model = imageUrl,
+        contentDescription = "Weather Icon",
+        modifier = Modifier.size(80.dp)
+    )
 }
