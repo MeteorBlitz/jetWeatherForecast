49fc07268ac1924adb8a6465e115521202e1f567
deepak <dlipkumar77@gmail.com>
2025-04-01 16:59:05 +0530

    search screen topAppbar & back pressed button

--- app/src/main/java/com/example/jetweatherforecast/screens/search/SearchScreen.kt
+++ app/src/main/java/com/example/jetweatherforecast/screens/search/SearchScreen.kt
@@ -1,16 +1,30 @@
 package com.example.jetweatherforecast.screens.search
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.jetweatherforecast.widgets.WeatherAppBar
 
 @Composable
 fun SearchScreen(navController: NavController) {
-    Scaffold(topBar = {}) { innerPadding ->
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = "Search",
+            navController = navController,
+            icon = Icons.AutoMirrored.Filled.ArrowBack,
+            isMainScreen = false,
+            elevation = 5.dp
+        ){
+            navController.popBackStack()
+        }
+    }) { innerPadding ->
         Surface(modifier = Modifier
             .padding(innerPadding)) {
             Text(text = "Search Screen")
