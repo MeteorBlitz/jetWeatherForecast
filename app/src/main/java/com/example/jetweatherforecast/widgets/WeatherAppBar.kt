b9800e0c1a14b5d1dfd61b386985f9d7309ccd1f
deepak <dlipkumar77@gmail.com>
2025-04-02 12:04:52 +0530

    topBar DropdownMenuItem dialog UI & click function setup

--- app/src/main/java/com/example/jetweatherforecast/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/example/jetweatherforecast/widgets/WeatherAppBar.kt
@@ -1,9 +1,21 @@
 package com.example.jetweatherforecast.widgets
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,6 +24,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
@@ -35,6 +51,12 @@ fun WeatherAppBar(
     onAddActionClicked: () -> Unit = {},
     onButtonClicked: () -> Unit = {},
 ) {
+    val showDialog = remember {
+        mutableStateOf(false)
+    }
+    if (showDialog.value) {
+        SettingsDropDownMenu(showDialog = showDialog, navController = navController)
+    }
     TopAppBar(title = {Text(title,
         color = MaterialTheme.colorScheme.onBackground,
         style = MaterialTheme.typography.titleLarge,
@@ -45,7 +67,7 @@ fun WeatherAppBar(
                 IconButton(onClick = { onAddActionClicked.invoke() }) {
                     Icon(imageVector = Icons.Default.Search, contentDescription = "search icon")
                 }
-                IconButton(onClick = { /*TODO*/ }) {
+                IconButton(onClick = { showDialog.value = true }) {
                     Icon(imageVector = Icons.Default.MoreVert, contentDescription = "more icon")
 
                 }
@@ -64,4 +86,51 @@ fun WeatherAppBar(
         ),
         modifier = Modifier.shadow(elevation=elevation))
 
+}
+
+@Composable
+fun SettingsDropDownMenu(showDialog: MutableState<Boolean>, navController: NavController) {
+    var expanded = remember {
+        mutableStateOf(true)
+    }
+    val items = listOf("About", "Favorites", "Settings")
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentSize(Alignment.TopEnd)
+        .absolutePadding(top = 45.dp, right = 20.dp)) {
+        DropdownMenu(expanded = expanded.value,
+            onDismissRequest = { expanded.value = false},
+            modifier = Modifier
+                .width(140.dp)
+                .background(Color.White)) {
+            items.forEachIndexed { index, text ->
+                DropdownMenuItem(
+                    onClick = {
+                        expanded.value = false
+                        showDialog.value = false
+                        when(text){
+                            "About" -> Log.d("TAG", "SettingsDropDownMenu: About menu clicked")
+                            "Favorites" -> Log.d("TAG", "SettingsDropDownMenu: Favorites menu clicked")
+                            "Settings" -> Log.d("TAG", "SettingsDropDownMenu: Settings menu clicked")
+                        }
+                    },
+                    leadingIcon = {
+                        val icon = when (text) {
+                            "About" -> Icons.Filled.Info
+                            "Favorites" -> Icons.Filled.Favorite
+                            else -> Icons.Default.Info
+                        }
+                        Icon(
+                            imageVector = icon,
+                            contentDescription = text,
+                            tint = Color.LightGray
+                        )
+                      },
+                    text = {Text(text = text)},
+                )
+            }
+
+        }
+
+    }
 }
