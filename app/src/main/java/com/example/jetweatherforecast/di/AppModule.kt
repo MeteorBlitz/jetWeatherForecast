c99b7c2c971bef4a103761533057e63dfd2bec0d
deepak <dlipkumar77@gmail.com>
2025-04-02 13:48:51 +0530

    ROOM - connect DI (App module -> database to access whenever we need)

--- app/src/main/java/com/example/jetweatherforecast/di/AppModule.kt
+++ app/src/main/java/com/example/jetweatherforecast/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.example.jetweatherforecast.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.jetweatherforecast.data.WeatherDao
+import com.example.jetweatherforecast.data.WeatherDatabase
 import com.example.jetweatherforecast.network.WeatherApi
 import com.example.jetweatherforecast.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -13,6 +18,20 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideWeatherDao(weatherDatabase: WeatherDatabase): WeatherDao
+    = weatherDatabase.weatherDao()
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context : Context): WeatherDatabase
+    = Room.databaseBuilder(
+        context,
+        WeatherDatabase::class.java,
+        "weather_database"
+    ).fallbackToDestructiveMigration().build()
 
     @Provides
     @Singleton
